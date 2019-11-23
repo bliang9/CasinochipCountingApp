6aac193b49f9c3438607fc67777d9a15fe1523f7
bliang9 <bwl3@illinois.edu>
2019-11-22 22:58:08 -0600

    Test

--- app/src/main/java/com/example/casinochipcountingapp/JoinGameActivity.java
+++ app/src/main/java/com/example/casinochipcountingapp/JoinGameActivity.java
@@ -1,4 +1,24 @@
 package com.example.casinochipcountingapp;
 
-public class JoinGameActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JoinGameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joingame);
+        final TextView typeGameId = findViewById(R.id.typeGameId);
+        Button joinGame = findViewById(R.id.joinGame);
+        joinGame.setOnClickListener(unused -> joinTheGame(typeGameId.getText().toString()));
+        Button createGame = findViewById(R.id.createGame);
+    }
+    public void joinTheGame(String gameId) {
+        if (!gameId.equals("")) {
+
+        }
+    }
 }
