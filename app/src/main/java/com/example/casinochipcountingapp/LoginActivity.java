05a81d5e8ea60d80a8288818830adc719ebe568d
bliang9 <bwl3@illinois.edu>
2019-11-28 17:30:28 -0600

    c

--- app/src/main/java/com/example/casinochipcountingapp/LoginActivity.java
+++ app/src/main/java/com/example/casinochipcountingapp/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.casinochipcountingapp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,14 +18,31 @@ public class LoginActivity extends AppCompatActivity {
         Button login = findViewById(R.id.login);
         login.setOnClickListener(unused -> loginIntoAccount());
         typeinEmail = findViewById(R.id.typeinEmail);
+        typeinEmail.setTextColor(Color.GREEN);
+        TextView email = findViewById(R.id.email);
+        email.setTextColor(Color.GRAY);
+        TextView password = findViewById(R.id.password);
+        password.setTextColor(Color.GRAY);
         typeinPassword = findViewById(R.id.typeinPassword);
-
+        typeinPassword.setTextColor(Color.GREEN);
+        Button createAccount = findViewById(R.id.createAccount);
+        createAccount.setOnClickListener(unused -> createAccount());
     }
     public void loginIntoAccount() {
         //
         String email = typeinEmail.getText().toString();
         String password = typeinPassword.getText().toString();
+        if (email != null && password != null) {
+            Intent intent = new Intent(this, JoinGameActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
 
+    }
+    public void createAccount() {
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
