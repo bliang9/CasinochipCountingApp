51a409d394948e47db03999554eac9e85862a52d
jiaruil4 <jiaruil4@illinois.edu>
2019-12-05 23:59:27 -0600

    minus button and total amount counting

--- app/src/main/java/com/example/casinochipcountingapp/GameActivity.java
+++ app/src/main/java/com/example/casinochipcountingapp/GameActivity.java
@@ -16,11 +16,17 @@ public class GameActivity extends AppCompatActivity {
     * The total amount of player chips.
      */
     private int playerChipAmount;
+    private TextView playerAmount;
     private Button add;
+    private Button minus;
     private TextView addtextView;
+    private TextView minustextView;
     private EditText addAmount;
+    private EditText minusAmount;
     private Button addAgree;
-    private AlertDialog dialog;
+    private Button minusAgree;
+    private AlertDialog adddialog;
+    private AlertDialog minusdialog;
     /**
      *
      * @param savedInstanceState
@@ -30,6 +36,8 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         add = findViewById(R.id.add);
+        minus = findViewById(R.id.minus);
+        playerAmount = findViewById(R.id.playerAmount);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,14 +50,38 @@ public class GameActivity extends AppCompatActivity {
 
 
                 addAgree.setOnClickListener(unused -> addChips());
+
                 mBuilder.setView(mView);
                 AlertDialog k = mBuilder.create();
-                dialog = k;
-                dialog.show();
+                adddialog = k;
+                adddialog.show();
                 addAgree.setOnClickListener(unused -> addChips());
             }
 
         });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(GameActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_minus, null);
+                minusAmount = mView.findViewById(R.id.minusAmount);
+                minusAgree = mView.findViewById(R.id.minusAgree);
+                minustextView = mView.findViewById(R.id.minustextView);
+
+
+                minusAgree.setOnClickListener(unused -> minusChips());
+                mBuilder.setView(mView);
+                AlertDialog k = mBuilder.create();
+                minusdialog = k;
+                minusdialog.show();
+                minusAgree.setOnClickListener(unused -> minusChips());
+            }
+
+        });
+
+
+
     }
     /**
      * Called when user is adding chips.
@@ -58,12 +90,46 @@ public class GameActivity extends AppCompatActivity {
         String typein = addAmount.getText().toString();
         if (isNumeric(typein)) {
             int addamount = Integer.parseInt(typein);
-            playerChipAmount += addamount;
-            Toast.makeText(GameActivity.this,
-                    "Add Successful",
-                    Toast.LENGTH_SHORT).show();
+            if (addamount % 5 !=0) {
+                Toast.makeText(GameActivity.this,
+                        "Number has to be the Multiples of 5",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                playerChipAmount += addamount;
+                String amount = Integer.toString(playerChipAmount);
+                ((TextView) findViewById(R.id.playerAmount)).setText(amount);
+                Toast.makeText(GameActivity.this,
+                        "Add Successful",
+                        Toast.LENGTH_SHORT).show();
 
-            dialog.cancel();
+                adddialog.cancel();
+            }
+
+        } else {
+            Toast.makeText(GameActivity.this,
+                    "Only Type In Number",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void minusChips() {
+        String typein = minusAmount.getText().toString();
+        if (isNumeric(typein)) {
+            int minusamount = Integer.parseInt(typein);
+            if (minusamount % 5 !=0) {
+                Toast.makeText(GameActivity.this,
+                        "Number has to be the Multiples of 5",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                playerChipAmount -= minusamount;
+                String amount = Integer.toString(playerChipAmount);
+                ((TextView) findViewById(R.id.playerAmount)).setText(amount);
+                Toast.makeText(GameActivity.this,
+                        "Minus Successful",
+                        Toast.LENGTH_SHORT).show();
+
+                minusdialog.cancel();
+            }
 
         } else {
             Toast.makeText(GameActivity.this,
@@ -86,4 +152,5 @@ public class GameActivity extends AppCompatActivity {
         }
         return true;
     }
+
 }
